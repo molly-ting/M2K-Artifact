799edaa04469c1754bef0ab3a5acdce2ece4fe46
RegulusJia <1609918355@qq.com>
2026-07-09 09:53:22 +0800

    remove dead code

--- HFProbe/backend/config_agent_vllm.py
+++ HFProbe/backend/config_agent_vllm.py
@@ -180,8 +180,8 @@ def generate_vllm(model_structure, op_name, config_example, framework_config=Non
     
     if framework_config:
         vllm_config = None
-        if "vllm_config" in framework_config:
-            vllm_config = framework_config["vllm_config"]
+        if "vllmconfig" in framework_config:
+            vllm_config = framework_config["vllmconfig"]
         envs_config = None
         if "envs" in framework_config:
             envs_config = framework_config["envs"]
