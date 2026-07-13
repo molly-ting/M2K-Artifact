c92130caf6a043307c0b3e4ee9c2f3dab1ec3095
RegulusJia <1609918355@qq.com>
2026-07-13 12:35:10 +0800

    fix env.sh

--- env.sh
+++ env.sh
@@ -6,6 +6,6 @@ PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export LLVM_DIR=/usr/lib/llvm-13
 export SIGNED_CLANG_PATH="${PROJECT_DIR}/.deps/llvm-project/build/bin"
-export PATH="${PROJECT_DIR}/cuKLEE/build:${LLVM_DIR}/bin:${PATH}"
+export PATH="${PROJECT_DIR}/cuKLEE/build:${PROJECT_DIR}/cuKLEE/klee/build/bin:${LLVM_DIR}/bin:${PATH}"
 
 source "${PROJECT_DIR}/.venv/bin/activate"
